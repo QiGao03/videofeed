1cc178cca804c699306844500af21945487bfe0e
高琪 <gaoqi13@meituan.com>
2019-05-25 15:02:06 +0800

    create project

---
+++ app/src/main/java/com/md/videofeed/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.md.videofeed;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author qigao
+ * @since 2019-05-25.
+ */
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
